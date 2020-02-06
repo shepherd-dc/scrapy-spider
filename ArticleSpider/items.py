ad470c9b5f10d9b644c8855ac40bda894c39f90a
Shepherd <shepherd_dc@163.com>
2020-02-06 23:17:29 +0800

    crawl questions and answers @ zhihu.com

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -4,12 +4,13 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
+import datetime
 
 import scrapy
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, Identity, MapCompose, Join
 
-from ArticleSpider.utils.common import date_convert
+from ArticleSpider.utils.common import date_convert, extract_num
 
 
 class ArticleSpiderItem(scrapy.Item):
@@ -51,6 +52,83 @@ class ArticleSpiderItem(scrapy.Item):
         params.append(self.get('fav_nums', 0))
         params.append(self.get('tags', ''))
         params.append(self.get('content', ''))
+
+        return insert_sql, params
+
+
+class ZhihuQuestionItem(scrapy.Item):
+    #知乎的问题 item
+    zhihu_id = scrapy.Field()
+    topics = scrapy.Field(
+        output_processor=Join(separator=',')
+    )
+    url = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+    answer_num = scrapy.Field()
+    comments_num = scrapy.Field()
+    watch_user_num = scrapy.Field()
+    click_num = scrapy.Field()
+    crawl_time = scrapy.Field()
+
+    def get_insert_sql(self):
+        #插入知乎question表的sql语句
+        insert_sql = """
+            insert into zhihu_question(zhihu_id, topics, url, title, content, answer_num, comments_num,
+              watch_user_num, click_num, crawl_time
+              )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE content=VALUES(content), answer_num=VALUES(answer_num), comments_num=VALUES(comments_num),
+              watch_user_num=VALUES(watch_user_num), click_num=VALUES(click_num)
+        """
+        zhihu_id = self.get("zhihu_id", "")
+        topics = self.get("topics", "")
+        url = self.get("url", "")
+        title = self.get("title", "")
+        content = self.get("content", "")
+        answer_num = self.get("answer_num", 0)
+        comments_num = self.get("comments_num", 0)
+        watch_user_num = int(self.get("watch_user_num", 0))
+        click_num = int(self.get("click_num", 0))
+        crawl_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        params = (zhihu_id, topics, url, title, content, answer_num, comments_num,
+                  watch_user_num, click_num, crawl_time)
+
+        return insert_sql, params
+
+
+class ZhihuAnswerItem(scrapy.Item):
+    #知乎的问题回答item
+    zhihu_id = scrapy.Field()
+    url = scrapy.Field()
+    question_id = scrapy.Field()
+    author_id = scrapy.Field()
+    content = scrapy.Field()
+    praise_num = scrapy.Field()
+    comments_num = scrapy.Field()
+    create_time = scrapy.Field()
+    update_time = scrapy.Field()
+    crawl_time = scrapy.Field()
+
+    def get_insert_sql(self):
+        #插入知乎question表的sql语句
+        insert_sql = """
+            insert into zhihu_answer(zhihu_id, url, question_id, author_id, content, praise_num, comments_num,
+              create_time, update_time, crawl_time
+              ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+              ON DUPLICATE KEY UPDATE content=VALUES(content), comments_num=VALUES(comments_num), praise_num=VALUES(praise_num),
+              update_time=VALUES(update_time)
+        """
+
+        create_time = datetime.datetime.fromtimestamp(self["create_time"]).strftime('%Y-%m-%d %H:%M:%S')
+        update_time = datetime.datetime.fromtimestamp(self["update_time"]).strftime('%Y-%m-%d %H:%M:%S')
+        params = (
+            self["zhihu_id"], self["url"], self["question_id"],
+            self["author_id"], self["content"], self["praise_num"],
+            self["comments_num"], create_time, update_time,
+            self["crawl_time"].strftime('%Y-%m-%d %H:%M:%S'),
+        )
 
         return insert_sql, params
 
--- ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/spiders/zhihu.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
+import datetime
 import json
+import re
 import time
 from pathlib import Path
+from urllib import parse
 
 import scrapy
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+from ArticleSpider.items import ArticleItemLoader, ZhihuQuestionItem, ZhihuAnswerItem
 
 
 class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
+
+    #question的第1页answer的请求url
+    start_answer_url = "https://www.zhihu.com/api/v4/questions/{0}/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit={1}&offset={2}&platform=desktop&sort_by=default"
 
     # 模拟请求的headers，非常重要，不设置也可能知乎不让你访问请求
     headers = {
@@ -88,17 +96,76 @@ class ZhihuSpider(scrapy.Spider):
         jsonCookies = json.dumps(cookies)  # 通过json将cookies写入文件
         with open('zhihuCookies.json', 'w') as f:
             f.write(jsonCookies)
-        print(cookies)
+        # print(cookies)
 
     def parse(self, response):
-
         # 这里打印出https://www.zhihu.com/notifications页面中通知中心带上cookies登录完成
-        print("登录成功: %s" % response.xpath('//div[@class="Card Notifications-Main"]//h1/text()').extract_first())
+        # print("登录成功: %s" % response.xpath('//div[@class="Card Notifications-Main"]//h1/text()').extract_first())
 
-        print("*" * 100)
-        # print("response text: %s" % response.text)
-        print("response headers: %s" % response.headers)
-        print("response meta: %s" % response.meta)
-        print("request headers: %s" % response.request.headers)
-        print("request cookies: %s" % response.request.cookies)
-        print("request meta: %s" % response.request.meta)
+        """
+        提取出html页面中的所有url 并跟踪这些url进一步爬取
+        如果提取的url中格式为 /question/xxx 就下载之后直接进入解析函数
+        """
+        all_urls = response.css("a::attr(href)").extract()
+        all_urls = [parse.urljoin(response.url, url) for url in all_urls]
+        all_urls = filter(lambda x: True if x.startswith("https") else False, all_urls)
+        for url in all_urls:
+            match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", url)
+            if match_obj:
+                # 如果提取到question相关的页面则下载后交由提取函数进行提取
+                request_url = match_obj.group(1)
+                question_id = match_obj.group(2)
+                yield scrapy.Request(request_url, headers=self.headers, meta={"question_id": question_id}, callback=self.parse_question)
+            else:
+                # 如果不是question页面则直接进一步跟踪
+                # pass
+                yield scrapy.Request(url, headers=self.headers, callback=self.parse)
+
+    def parse_question(self, response):
+        # 处理question页面， 从页面中提取出具体的question item
+
+        question_id = response.meta.get('question_id', '')
+
+        item_loader = ArticleItemLoader(item=ZhihuQuestionItem(), response=response)
+        item_loader.add_css("title", "h1.QuestionHeader-title::text")
+        item_loader.add_css("content", ".QuestionHeader-detail")
+        item_loader.add_value("url", response.url)
+        item_loader.add_value("zhihu_id", question_id)
+        item_loader.add_css("answer_num", ".List-headerText span::text")
+        item_loader.add_css("comments_num", ".QuestionHeader-Comment span::text")
+        item_loader.add_css("watch_user_num", ".NumberBoard:first-child .NumberBoard-itemValue::text")
+        item_loader.add_css("click_num", ".NumberBoard:last-child .NumberBoard-itemValue::text")
+        item_loader.add_css("topics", ".QuestionHeader-topics .Popover div::text")
+
+        question_item = item_loader.load_item()
+
+        # 请求答案api
+        answer_url = self.start_answer_url.format(question_id, 5, 0)
+        yield scrapy.Request(answer_url, headers=self.headers, callback=self.parse_answer)
+
+        yield question_item
+
+    def parse_answer(self, response):
+        # 处理question的answer
+        answer_json = json.loads(response.text)
+        is_end = answer_json["paging"]["is_end"]
+        next_url = answer_json["paging"]["next"]
+
+        # 提取answer的具体字段
+        for answer in answer_json["data"]:
+            answer_item = ZhihuAnswerItem()
+            answer_item["zhihu_id"] = answer["id"]
+            answer_item["url"] = answer["url"]
+            answer_item["question_id"] = answer["question"]["id"]
+            answer_item["author_id"] = answer["author"]["id"] if "id" in answer["author"] else None
+            answer_item["content"] = answer["content"] if "content" in answer else None
+            answer_item["praise_num"] = answer["voteup_count"]
+            answer_item["comments_num"] = answer["comment_count"]
+            answer_item["create_time"] = answer["created_time"]
+            answer_item["update_time"] = answer["updated_time"]
+            answer_item["crawl_time"] = datetime.datetime.now()
+
+            yield answer_item
+
+        if not is_end:
+            yield scrapy.Request(next_url, headers=self.headers, callback=self.parse_answer)
