58f7bfde6bda6d43962a5c63abec1462e312e3dc
Shepherd <shepherd_dc@163.com>
2020-02-09 16:32:15 +0800

    selenium auto scroll

--- tools/selenium_spider.py
+++ tools/selenium_spider.py
@@ -22,11 +22,28 @@ browser = webdriver.Chrome(executable_path='C:/scrapy/chromedriver.exe')
 # browser.find_element_by_css_selector("#sign-in-form-submit-btn").click()
 # browser.find_element_by_css_selector("#sign-in-form-submit-btn").click()
 
-browser.get('https://account.cnblogs.com/signin')
-browser.find_element_by_css_selector("input#LoginName").send_keys("shepherd_nt")
-browser.find_element_by_css_selector("input#Password").send_keys("linfeng0328")
-browser.find_element_by_css_selector("#submitBtn").click()
+# browser.get('https://account.cnblogs.com/signin')
+# time.sleep(3) # 等待页面加载完成，防止下面的操作找不到原素
+# browser.find_element_by_css_selector("input#LoginName").clear()
+# browser.find_element_by_css_selector("input#LoginName").send_keys("shepherd_nt")
+# browser.find_element_by_css_selector("input#Password").clear()
+# browser.find_element_by_css_selector("input#Password").send_keys("linfeng0328")
+# browser.find_element_by_css_selector("#submitBtn").click()
+#
+# print('yes')
+#
+# time.sleep(60)
 
-print('yes')
+browser.get('https://www.oschina.net/blog')
+time.sleep(3)
 
-time.sleep(60)
+js_script = 'window.scrollTo(0, document.body.scrollHeight); var lenOfPage = document.body.scrollHeight; return lenOfPage;'
+lenOfPage = browser.execute_script(js_script)
+
+match=False
+while(match == False):
+    lastCount = lenOfPage
+    time.sleep(3)
+    lenOfPage = browser.execute_script(js_script)
+    if lastCount == lenOfPage:
+        match = True
